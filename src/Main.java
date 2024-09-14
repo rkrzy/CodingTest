42f17d2fca0753b7d3bc8e0fdce0930a302fd94b
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-09-15 08:53:10 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int N;
+        int result = 0;
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        for(int i = 0;i<N;i++)
+        {
+
+        }
+    }
+}
