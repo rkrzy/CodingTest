dec292e1bfa5daebad9e0372ae52cea49da1a731
임준식 <81816761+rkrzy@users.noreply.github.com>
2024-11-17 11:34:08 +0900

    [Silver V] Title: 최대 상승, Time: 1028 ms, Memory: 182516 KB -BaekjoonHub

---
+++ 백준/Silver/25644. 최대 상승/최대 상승.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       Scanner scanner = new Scanner(System.in);
+       int back;
+       int front;
+       int N;
+       int result = 0;
+       N = scanner.nextInt();
+       int []arr = new int[N];
+       for(int i = 0; i <N;i++)
+       {
+           arr[i] = scanner.nextInt();
+       }
+       back = arr[0];
+       arr[0] = 0;
+       for(int i = 1; i < N; i++)
+       {
+           front = arr[i];
+
+           if(front - back > 0)
+           {
+               arr[i] = front - back;
+           }
+           else
+           {
+               arr[i] = 0;
+           }
+           back = back<front ? back : front;
+       }
+       for(int j = 0;j<N;j++)
+       {
+           if(result < arr[j])
+           {
+               result = arr[j];
+           }
+       }
+       System.out.println(result);
+    }
+
+}
