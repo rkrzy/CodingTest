0bd07d7011ac284aa64ae42895c014787b870151
임준식 <81816761+rkrzy@users.noreply.github.com>
2025-01-16 01:45:15 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String[] str = br.readLine().split(" ");
+
+        int v = Integer.parseInt(br.readLine());
+
+        int[] save = new int[201];
+
+        for(int i = 0;i<n;i++){
+            int temp = Integer.parseInt(str[i]);
+            save[temp + 100]++;
+        }
+        System.out.println(save[v+ 100]);
+
+
+    }
+}
