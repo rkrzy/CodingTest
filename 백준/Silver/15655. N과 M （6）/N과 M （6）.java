ab64075676cd9973ddc4657eef86a9977c9ae6f8
임준식 <81816761+rkrzy@users.noreply.github.com>
2025-01-25 14:34:35 +0900

    [Silver III] Title: N과 M (6), Time: 128 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] numbers;
+    static int[] save;
+    static boolean[] visit;
+    public static void func1(int depth, int start) {
+        if (depth == M) {
+            for(int i = 0; i < M ;i ++){
+                System.out.print(save[i] + " ");
+            }
+            System.out.println();
+        }
+        for(int i = start;i<N;i++){
+            if(!visit[i]){
+                save[depth] = numbers[i];
+                visit[i] = true;
+                func1(depth+1, i+1);
+                visit[i] = false;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        N = Integer.parseInt(str[0]);
+        M = Integer.parseInt(str[1]);
+        numbers = new int[N];
+        visit = new boolean[N];
+        save = new int[N];
+        str = br.readLine().split(" ");
+        for(int i = 0; i < N;i++)
+        {
+            numbers[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(numbers);
+        func1(0, 0);
+    }
+}
