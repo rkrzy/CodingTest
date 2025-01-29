80d7111270a73033f03ff455cdd982e206dbee4d
임준식 <81816761+rkrzy@users.noreply.github.com>
2025-01-29 18:39:11 +0900

    [Silver IV] Title: 카드, Time: 368 ms, Memory: 33296 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.util.logging.Logger;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<Long, Integer> map = new HashMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            Long str = Long.parseLong(br.readLine());
+            if(map.containsKey(str)){
+                int num = map.get(str);
+                map.put(str,num +1);
+            }
+            else {
+                map.put(str,1);
+            }
+        }
+       List<Map.Entry<Long,Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort(Map.Entry.comparingByKey());
+        entryList.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+
+        System.out.println(entryList.get(0).getKey());
+    }
+}
