635ae76254eaccea8d06dd61355396335fe50219
임준식 <81816761+rkrzy@users.noreply.github.com>
2025-02-07 18:30:20 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,31 +1,32 @@
 import java.io.*;
+import java.util.Arrays;
 
+/*
+1. 테이블 정의하기
+dp[i] = 0으로 시작하지 않고, 1이 두번 연속으로 나오지 않는 i자리의 수
+2. 점화식 찾기 :dp[i] = dp[i-2] + dp[i-1]
+3. 초기값 정의하기 :
+dp[1] = 1
+dp[2] = 1
+dp[3] = 2
 
+ */
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
 
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       int N = Integer.parseInt(br.readLine());
-       long[] arr = new long[N+1];
-       long num0 = 1;
-       long num1 = 1;
-       long temp = 0;
-       arr[1] = 1;
-       if(N >=2)
-       {
-           arr[2] = 1;
-       }
-       for(int i = 3;i<=N;i++)
-       {
-           arr[i] = num0 + num1;
-           temp = num0;
-           num0 = num0 + num1;
-           num1 = temp;
-
-       }
-       System.out.println(arr[N]);
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[91];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+        for(int i = 4; i <= n; i++){
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        System.out.println(dp[n]);
     }
 }
