35ba8919cdfdbf5e0747a840c5fe91dde545f946
임준식 <81816761+rkrzy@users.noreply.github.com>
2025-03-07 19:37:18 +0900

    [Silver V] Title: 뒤집기, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,41 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Main {
-    public static void main(String[] args) {
 
-        int []arr = new int[2];
-        Scanner scanner = new Scanner(System.in);
-        String str;
-        str = scanner.next();
-        arr[0] = 0;
-        arr[1] = 0;
-        if(str.charAt(0) == '0')
-        {
-            arr[0]++;
-        }
-        else
-        {
-            arr[1]++;
-        }
-        for(int i = 1;i<str.length();i++)
-        {
-            if(str.charAt(i-1) == '0'&&str.charAt(i) == '1')
-            {
-                arr[1]++;
-            }
-            if(str.charAt(i-1) == '1' && str.charAt(i) == '0')
-            {
-                arr[0]++;
-            }
-        }
-        if(arr[0]>arr[1])
-        {
-            System.out.println(arr[1]);
-        }
-        else
-        {
-            System.out.println(arr[0]);
-        }
+
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       String str = br.readLine();
+
+       int result1 = 0;
+       int result0 = 0;
+       char pre = ' ';
+       char cur = ' ';
+       if(str.charAt(0) == '0'){
+           result0++;
+       }
+       else {
+           result1++;
+       }
+       pre = str.charAt(0);
+       for(int i = 1; i < str.length(); i++){
+           cur = str.charAt(i);
+           if(pre != cur){
+               pre = cur;
+               if(cur == '0'){
+                   result0++;
+               }
+               else{
+                   result1++;
+               }
+           }
+       }
+
+       System.out.println(result1 > result0? result0 : result1);
 
     }
+
 }
