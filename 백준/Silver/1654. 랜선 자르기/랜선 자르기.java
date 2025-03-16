061d9448773b658b920d0e315b5e8fe4797e8f85
임준식 <81816761+rkrzy@users.noreply.github.com>
2025-03-16 19:48:15 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 16844 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -25,29 +25,26 @@ public class Main {
                 max = arr[i];
             }
         }
-        max++;
 
-        long min = 0;
+        long min = 1;
         long mid = 0;
 
-        while(min < max) {
+        while(min <= max) {
             mid = (max + min) / 2;
-
             long count = 0;
-
             for(int i = 0; i < arr.length; i++){
                 count += (arr[i] / mid);
             }
 
             if(count < N){
-                max = mid;
+                max = mid - 1;
             }
             else{
                 min = mid + 1;
             }
         }
 
-        System.out.println(min - 1);
+        System.out.println(max);
     }
 
 }
