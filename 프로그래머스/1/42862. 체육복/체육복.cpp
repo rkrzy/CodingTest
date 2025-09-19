22d0c05e371aa5976d0713abc541b093b70e5967
임준식 <81816761+rkrzy@users.noreply.github.com>
2025-09-20 00:28:40 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -4,47 +4,22 @@
 
 using namespace std;
 
-
+int student[35];
 int solution(int n, vector<int> lost, vector<int> reserve) {
-    
-    vector<int> save_lost;
-    vector<int> save_reserve;
-    
-    sort(reserve.begin(), reserve.end());
-    sort(lost.begin(), lost.end());
-    
-    for(int i : lost){
-        auto it = find(reserve.begin(), reserve.end(), i);
-        if(it ==reserve.end()){
-            save_lost.push_back(i);
+    int answer = 0;
+    for(int i : reserve) student[i] += 1;
+    for(int i : lost) student[i] -= 1;
+    for(int i = 1; i <= n; i++) {
+        if(student[i] == -1){
+            if(student[i-1] == 1){
+                student[i-1] = student[i] = 0;
+            }else if(student[i+1]==1){
+                student[i] = student[i+1] =0;
+            }
         }
     }
-    for(int i : reserve){
-        auto it = find(lost.begin(), lost.end(), i);
-        if(it == lost.end()){
-            save_reserve.push_back(i);
-        }
-        
+    for(int i = 1; i <= n; i++){
+        if(student[i] != -1) answer++;
     }
-    
-    
-    int answer = n - save_lost.size(); 
-    
-    for(int i : save_lost){
-        auto it = find(save_reserve.begin(), save_reserve.end(), i-1);
-        if(it != save_reserve.end()){
-            answer++;
-            *it = -1;
-            continue;
-        }
-        it = find(save_reserve.begin(), save_reserve.end(), i+1);
-        if(it != save_reserve.end()){
-            answer++;
-            *it = -1;
-        }
-    }
-     
-   
-        
-    return answer;
+    return answer++;
 }
