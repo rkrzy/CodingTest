8fbbcd48516095338e26e3dbc8dc3ecbc647e305
임준식 <81816761+rkrzy@users.noreply.github.com>
2025-09-20 09:57:38 +0900

    [level 1] Title: 옹알이 (2), Time: 0.08 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -5,40 +5,18 @@ using namespace std;
 
 int solution(vector<string> babbling) {
     int answer = 0;
-    for(string str : babbling){
-        int index = 0;
-        bool flag = true;
-        vector<bool> chance(4, false);
-        while(index < str.length()){
-            if(str.length() < 2){
-                flag = false;
-                break;
+    for(int i = 0; i < babbling.size(); i++){
+        string temp1 = "";
+        string temp2 = "";
+        for(char c : babbling[i]){
+            temp1+=c;
+            if(temp1 == "aya" || temp1 == "ye" || temp1 == "woo" || temp1 == "ma"){
+                if(temp2 == temp1) break;
+                temp2 = temp1;
+                temp1 ="";
             }
-            if(str.substr(index, 3) == "aya" && !chance[0]){
-                index += 3;
-                fill(chance.begin(), chance.end(),0);
-                chance[0] = true;
-            }else if(str.substr(index, 2) == "ye"&& !chance[1]){
-                index += 2;
-                fill(chance.begin(), chance.end(),0);
-                chance[1] = true;
-            }else if(str.substr(index,3) == "woo"&& !chance[2]){
-                index += 3;
-                fill(chance.begin(), chance.end(),0);
-                chance[2] = true;
-            }else if(str.substr(index, 2) == "ma"&& !chance[3]){
-                index += 2;
-                fill(chance.begin(), chance.end(),0);
-                chance[3] = true;
-            }else{
-                flag = false;
-                break;
-            }
-            
         }
-        if(flag){
-            answer++;
-        }
+        if(temp1.size() == 0) answer++;
     }
     return answer;
 }
